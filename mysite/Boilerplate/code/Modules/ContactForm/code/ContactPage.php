cfa7c3ff2eda80c22788f7c9f2a81032f04641ae
Rhym <contact@ryanpotter.co.nz>
2013-12-20 11:36:34 +1300

    Contact Form

--- mysite/Boilerplate/code/Modules/ContactForm/code/ContactPage.php
+++ mysite/Boilerplate/code/Modules/ContactForm/code/ContactPage.php
@@ -6,7 +6,13 @@ class ContactPage extends Page {
 
     public static $db = array(
         'Mailto' => 'Varchar(100)',
-        'SubmitText' => 'Text'
+        'SubmitText' => 'Text',
+        'GoogleAPI' => 'Varchar(255)',
+        'Latitude' => 'Varchar(255)',
+        'Longitude' => 'Varchar(255)',
+        'MapColor' => 'Varchar(255)',
+        'WaterColor' => 'Varchar(255)',
+        'MapMarker' => 'Boolean(1)'
     );
 
     public static $defaults = array(
@@ -16,6 +22,27 @@ class ContactPage extends Page {
     public function getCMSFields() {
 
         $fields = parent::getCMSFields();
+
+        Requirements::javascript('mysite/Boilerplate/javascript/colorpicker.min.js');
+		Requirements::css('mysite/Boilerplate/css/colorpicker.css');
+
+        $fields->addFieldToTab('Root.Main', new LiteralField('js',
+		'<script type="text/javascript">
+			(function($) {
+				$(document).ready(function() {
+					$(\'.color-picker\').on(\'click\', function(){
+						$(this).iris({
+							hide: false,
+							change: function(event, ui) {
+								var $c, $r, $g, $b, $mid;
+								$(this).css(\'backgroundColor\', ui.color.toString());
+							}
+						});
+					});
+				});
+			})(jQuery);
+		</script>'
+		));
 
         /* =========================================
          * Settings
@@ -29,6 +56,24 @@ class ContactPage extends Page {
         $fields->addFieldToTab("Root.Main", new TextField("Mailto", "Choose an email address for the contact page to send to"), 'Content');
         $fields->addFieldToTab("Root.Main", new TextareaField("SubmitText", "Text for contact form submission"), 'Content');
 
+        /* -----------------------------------------
+         * Google Map
+        ------------------------------------------*/
+
+        $toggleFields = ToggleCompositeField::create(
+			'GoogleMap',
+			'Google Map',
+			array(
+                new Textfield('GoogleAPI', 'Maps API (Optional)'),
+                new Textfield('Latitude', 'Latitude'),
+                new Textfield('Longitude', 'Longitude'),
+                new ColorField('MapColor', 'Map Colour (Optional)'),
+                new ColorField('WaterColor', 'Water Colour (Optional)'),
+                new CheckboxField('MapMarker', 'Map Marker (Optional)'),
+			)
+		)->setHeadingLevel(4)->setStartClosed(true);
+		$fields->addFieldToTab('Root.Main', $toggleFields, 'Content');
+
         return $fields;
 
     }
@@ -38,6 +83,38 @@ class ContactPage extends Page {
 class ContactPage_Controller extends Page_Controller {
 
     private static $allowed_actions = array('ContactForm');
+
+    public function init() {
+
+		parent::init();
+
+        if($this->Latitude && $this->Longitude){
+
+            if($this->MapColor != ''){
+                $mapColor = "'".$this->MapColor."'";
+            }else{
+                $mapColor = 'false';
+            }
+            if($this->WaterColor != ''){
+                $waterColor = "'".$this->WaterColor."'";
+            }else{
+                $waterColor = 'false';
+            }
+            Requirements::javascript('https://maps.googleapis.com/maps/api/js?sensor=false');
+            Requirements::javascript('mysite/Boilerplate/code/Modules/ContactForm/js/mapScript.js');
+            Requirements::customScript(<<<JS
+(function($) {
+    $(document).ready(function(){
+        getMap($this->Latitude, $this->Longitude, $mapColor, $waterColor, $this->MapMarker)
+    })
+})(jQuery);
+JS
+);
+
+        }
+
+
+	}
 
     public function ContactForm() {
 
@@ -87,6 +164,7 @@ class ContactPage_Controller extends Page_Controller {
         $email->setTemplate('ContactEmail');
         $email->populateTemplate($data);
         $email->send();
+        $this->setFlash($this->SubmitText, 'success');
         return $this->redirect($this->Link("?success=1"));
 
     }
