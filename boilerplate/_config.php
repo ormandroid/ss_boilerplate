18f615fcb90df9cfa37dec7445a470381d4bce39
root <root@pantry.local>
2014-09-10 13:45:46 +1200

    Removed tinymce plugin config

--- boilerplate/_config.php
+++ boilerplate/_config.php
@@ -6,8 +6,8 @@ if (basename(dirname(__FILE__)) != BOILERPLATE_MODULE) {
     throw new Exception(BOILERPLATE_MODULE . ' module not installed in correct directory');
 }
 
-// TinyMCE plugin
-HtmlEditorConfig::get('cms')->enablePlugins(array(
-    'shortcodeGenerator' => Director::absoluteBaseURL().'boilerplate/javascript/shortcodeGenerator/editor_plugin.js'
-));
-HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'shortcodeGenerator');
+//// TinyMCE plugin
+//HtmlEditorConfig::get('cms')->enablePlugins(array(
+//    'shortcodeGenerator' => Director::absoluteBaseURL().'boilerplate/javascript/shortcodeGenerator/editor_plugin.js'
+//));
+//HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'shortcodeGenerator');
